29358df94d2a5bae8a65f19306d8010217959946
Hemant jha <jhah47777@gmail.com>
2025-12-28 01:47:42 +0530

    Cros-server-error

--- component/RestaurantMenuShimmer.js
+++ component/RestaurantMenuShimmer.js
@@ -16,9 +16,10 @@ export default function RestaurantMenuShimmer() {
   useEffect(() => {
     async function fetchData() {
       setLoading(true);
-      const proxyServer = "https://cors-anywhere.herokuapp.com/";
-      const swiggyAPI = `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=${id}`;
-      const response = await fetch(proxyServer + swiggyAPI);
+      // const proxyServer = "https://cors-anywhere.herokuapp.com/";
+      // const swiggyAPI = `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7040592&lng=77.10249019999999&restaurantId=${id}`;
+      // const response = await fetch(proxyServer + swiggyAPI);
+       const response = await fetch("/.netlify/functions/swiggy");
       const data = await response.json();
       const tempData =
         data?.data?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
